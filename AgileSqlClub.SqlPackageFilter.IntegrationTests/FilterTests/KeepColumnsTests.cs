7de48d3088b6559acdd09b0d17adf8acfad24e7e
Mykhailo Baranov <mykhailo.baranov@epam.com>
2022-09-13 13:09:27 +0300

    Added positive test for keep all statement

--- AgileSqlClub.SqlPackageFilter.IntegrationTests/FilterTests/KeepColumnsTests.cs
+++ AgileSqlClub.SqlPackageFilter.IntegrationTests/FilterTests/KeepColumnsTests.cs
@@ -259,5 +259,26 @@ namespace AgileSqlClub.SqlPackageFilter.IntegrationTests
                     "SELECT COUNT(*) FROM sys.columns where name = 'ohwahweewah' and object_id = object_id('Employees');");
             Assert.AreEqual(1, count, proc.Messages);
         }
+
+        [Test]
+        public void Column_Is_Added_When_Keep_all_Is_Used()
+        {
+            _gateway.RunQuery(
+                " exec sp_executesql N'drop table employees; create table Employees([EmployeeId] INT NOT NULL PRIMARY KEY); ';");
+
+            var args =
+                $"/Action:Publish /TargetConnectionString:\"{_connectionString}\" /SourceFile:{Path.Combine(TestContext.CurrentContext.TestDirectory, "Dacpac.Dacpac")} /p:AdditionalDeploymentContributors=AgileSqlClub.DeploymentFilterContributor " +
+                " /p:DropObjectsNotInSource=False " +
+                "/p:AdditionalDeploymentContributorArguments=\"SqlPackageFilter=KeepTableColumns(all)\" /p:AllowIncompatiblePlatform=true /p:GenerateSmartDefaults=true";
+
+            var proc = new ProcessGateway(Path.Combine(TestContext.CurrentContext.TestDirectory, "SqlPackage.exe\\SqlPackage.exe"), args);
+            proc.Run();
+            proc.WasDeploySuccess();
+
+            var count =
+                _gateway.GetInt(
+                    "SELECT COUNT(*) FROM sys.columns where name = 'Name' and object_id = object_id('Employees');");
+            Assert.AreEqual(1, count, proc.Messages);
+        }
     }
 }
